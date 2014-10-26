8775bc41a94366871a811003667dceb084ecf049
ymattsun <ymattsun28@gmail.com>
2014-10-27 02:03:14 +0900

    first commit

---
+++ 2_012_NOW.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys
+
+def NOW(FILE):
+  f = open(FILE)
+  for line in f:
+    line = line.strip().decode('utf-8')
+    if line[-2:] == u'なう':
+      sys.stdout.write('%s\n' % line.encode('utf-8'))
+  f.close()
+
+if __name__ == '__main__':
+  NOW(sys.argv[1])
