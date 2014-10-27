e2e515261aeef8e2642cf4ba3e6e331f0cb2adff
ymattsun <ymattsun28@gmail.com>
2014-10-27 16:31:33 +0900

    first commit

---
+++ 2_019_Post.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+import sys, re
+
+def Post_Number(FILE):
+  from collections import defaultdict
+
+  Post_Number = defaultdict(lambda: [])
+  for line in open(FILE):
+    #line = line.strip().decode('utf-8')
+    #cols = line.split()
+    #if len(cols) != 2:
+    #  continue
+    #glb, loc = cols
+    #if u'[0-9]-[0-9]' in glb:
+    #  pref, zone = glb.split(u'[0-9]-[0-9]')
+
+  return Post_Number
+
+def pref_name(FILE):
+  
+
+
+
+def town_name(FILE):
+
+
+
+if __name__ == '__main__':
+  #adrDict = Post_Number(sys.argv[2])
+  #for line in open(sys.argv[1]):
+  #  line = line.strip().decode('utf-8')
