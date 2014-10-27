97de842430cb332e32597816d240826a17d77be4
ymattsun <ymattsun28@gmail.com>
2014-10-27 23:50:25 +0900

    Update 2_020_Emoji.py

--- 2_020_Emoji.py
+++ 2_020_Emoji.py
@@ -4,15 +4,18 @@
 import sys, re
 
 def emoji(file_line):
-  emoji_kigou_pattern = re.compile(ur'([[(]（]+)(.+?)([[)]|）]+)')
-  #kanji_pattern = re.compile(ur'[一-龠]+')
-  result = re.match(emoji_kigou_pattern, file_line)
+  left_parts = re.compile(ur'([\]*)([(]*)')
+  face_parts = re.compile(ur'([一-龠]+)[^\"!?<>+#$%&~|][a-zA-Z0-9]')
+  right_parts = re.compile(ur'([)]*)([/]*)')
+  
+  kaomoji = left_parts + face_parts + right_parts
+  result = kaomoji.search(file_line)
   if result != None:
-    return emoji_kigou_pattern
+    return result.groups()
 
 if __name__ == '__main__':
-  emojis = {}
   for line in open(sys.argv[1]):
-    #line = line.strip().decode('utf-8')
-    #emojis = emoji(line)
-    #print ''.join(emojis).encode('utf-8')
+    line = line.strip().decode('utf-8')
+    emoji_groups = emoji(line)
+    if emoji_groups != None:
+      print emoji_groups
