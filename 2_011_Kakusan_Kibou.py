6fc300e7702242a1870ca13fa59d1f3c20c1b3f5
ymattsun <ymattsun28@gmail.com>
2014-10-30 09:47:36 +0900

    Update 2_011_Kakusan_Kibou.py

--- 2_011_Kakusan_Kibou.py
+++ 2_011_Kakusan_Kibou.py
@@ -9,7 +9,6 @@ def Kakusan_Kibou(FILE):
     line = line.decode('utf-8')
     if u'拡散希望' in line:
       sys.stdout.write(line.encode('utf-8'))
-  f.close()
 
 if __name__ == '__main__':
   Kakusan_Kibou(sys.argv[1])
