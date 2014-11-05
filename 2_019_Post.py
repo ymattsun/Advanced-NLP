7e8a04e3f73f9c528ecccda69a2d3a961638ea9e
ymattsun <ymattsun28@gmail.com>
2014-11-05 19:22:58 +0900

    Update 2_019_Post.py

--- 2_019_Post.py
+++ 2_019_Post.py
@@ -4,18 +4,19 @@
 import sys, re
 
 def post_pref_town(FILE):
-  post_num, pref_name, town_name = [], [], []
   for line in open(FILE):
     line = line.strip().decode('utf-8')
-    cols = line.split()
-    if ur'([0-9]+)-([0-9]+)' in cols:
-      post_num.append(cols)
-    if ur'都|道|府|県|' in cols:
-      pref_name.append(cols)
-    if ur'区|市|町|村|郡' in cols:
-      town_name.append(cols)
-  
-  post_pref_town_name = post_num + pref_name + town_name
+    #post_pref_town = re.compile(ur'([0-9]{3}-[0-9]{4})([一-龠]{2,3}(都|道|府|県|))(.{1,4}(区|市|町|村|郡))')
+    pref_town = re.compile(ur'([一-龠]{2,3}(都|道|府|県|))(.{1,4}(区|市|町|村|郡))')
+
+    #match_post_pref_town = post_pref_town.search(line)
+    match_pref_town = pref_town.search(line)
+
+    #if match_post_pref_town:
+      #print match_post_pref_town.group(0)
+
+    if match_pref_town:
+      print match_pref_town.group(0)
 
 if __name__ == '__main__':
   post_pref_town(sys.argv[1])
