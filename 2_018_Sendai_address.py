da29de925d0ac80a6a7edd93f01dd09d900b06d8
ymattsun <ymattsun28@gmail.com>
2014-11-05 16:08:18 +0900

    Update 2_018_Sendai_address.py

--- 2_018_Sendai_address.py
+++ 2_018_Sendai_address.py
@@ -4,15 +4,10 @@
 import sys, re
 
 def sendai_address(address_FILE):
-  sendai_add = []
   for line in open(address_FILE):
     line = line.strip().decode('utf-8')
-    cols = line.split()
-    if u'仙台市' in cols:
-      sendai_add.append(cols)
-
-  if len(sendai_add) != 0:
-    print sendai_add
+    if u'仙台市' in line:
+      print line.encode('utf-8') + '\n'
 
 if __name__ == '__main__':
   sendai_address(sys.argv[1])
