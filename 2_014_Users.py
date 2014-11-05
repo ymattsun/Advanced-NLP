492d591f31b1edce49f6a58359d4be8e7c947fbc
ymattsun <ymattsun28@gmail.com>
2014-11-05 14:45:50 +0900

    Update 2_014_Users.py

--- 2_014_Users.py
+++ 2_014_Users.py
@@ -4,14 +4,12 @@
 import sys, re
 
 def Users(FILE):
-  f = open(FILE)
   user = re.compile(ur'@[a-zA-Z0-9_]+')
-  users = [ user.findall(line) for line in f \
+  users = [ user.findall(line) for line in open(FILE) \
             if len(user.findall(line)) != 0 ]
-  f.close()
-  return users
+
+  for i in users:
+    print i
 
 if __name__ == '__main__':
-  users = Users(sys.argv[1])
-  for user in users:
-    print user
+  Users(sys.argv[1])
