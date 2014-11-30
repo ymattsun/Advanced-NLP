adc7846cb494d1adb8a9068be7994b5170aaed02
ymattsun <ymattsun28@gmail.com>
2014-11-30 21:56:28 +0900

    first commit

---
+++ 5_041_mecab.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys, re
+
+def mecab(FILE):
+  result = []
+  for line in open(FILE):
+    line = line.strip()
+    if line != 'EOS':
+      mecab_list = re.split(r'\t|,', line)
+      dict = {'surface': mecab_list[0], 'base' : mecab_list[7] , 'pos': mecab_list[1], 'pos1': mecab_list[2]}
+      result.append(dict)
+  
+  print result
+
+if __name__ == '__main__':
+  mecab(sys.argv[1])
