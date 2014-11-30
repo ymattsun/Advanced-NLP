e454bf89c6a4bc3ca866411847ccfffddcef9607
ymattsun <ymattsun28@gmail.com>
2014-11-30 21:59:04 +0900

    first commit

---
+++ 5_044_noun_sahen.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys, re
+
+def noun_sahen(FILE):
+  for line in open(FILE):
+    line = line.strip()
+    if line != 'EOS':
+      mecab_list = re.split(r'\t|,', line)
+      if mecab_list[2] == 'サ変接続':
+        print mecab_list[2] + mecab_list[0]
+
+if __name__ == '__main__':
+  noun_sahen(sys.argv[1])
