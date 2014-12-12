b7a3276d71edc6577bfdde0467f22d65cf045bc7
ymattsun <ymattsun28@gmail.com>
2014-12-12 17:05:51 +0900

    Update 6_055_tab_extract.py

--- 6_055_tab_extract.py
+++ 6_055_tab_extract.py
@@ -1,11 +1,23 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 import sys, re
 from module import *
 
 def tab_extract(FILE):
-  
+  chunk_list = module(FILE)
+
+  for chunk in chunk_list:
+    for i in range(len(chunk)):
+      if chunk[i].dst != '-1':
+        moto, saki = '', ''
+
+        for j1 in chunk[i].morphs:
+          if j1.pos != '記号':
+            moto += j1.surface
+
+        for j2 in chunk[int(chunk[i].dst)].morphs:
+          if j2.pos != '記号':
+            saki += j2.surface
+
+        print moto + '\t' + saki + '\n'
 
 if __name__ == '__main__':
   tab_extract(sys.argv[1])
