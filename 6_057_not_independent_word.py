4e4daf6b2d6c57a01d248df3ced410a76018b3a3
ymattsun <ymattsun28@gmail.com>
2014-12-12 17:07:14 +0900

    first commit

---
+++ 6_057_not_independent_word.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys, re
+from module import *
+
+def not_independent(FILE):
+  chunk_list = module(FILE)
+  f = open('not_independent.txt', 'w')
+
+  for chunk in chunk_list:
+    for i in range(len(chunk)):
+      if chunk[i].dst != '-1':
+        noun, verb = '', ''
+        noun_check, verb_check = 0, 0
+
+        for j1 in chunk[i].morphs:
+          if j1.surface != '、 ' and j1.surface != '。 ':
+            noun += j1.surface
+          if j1.pos == '名詞' and j1.pos1 != '非自立':
+            noun_check = 1
+
+        if noun_check == 0:
+          continue 
+
+        for j2 in chunk[int(chunk[i].dst)].morphs:
+          if j2.surface != '、 ' and j1.surface != '。 ':
+            verb += j2.surface
+          if j1.pos == '動詞' and j2.pos1 != '非自立':
+            verb_check = 1
+
+        if verb_check == 0:
+          continue
+
+        print noun + '\t' + verb + '\n'
+        f.write(noun + '\t' + verb + '\n')
+
+  f.close()
+
+if __name__ == '__main__':
+  not_independent(sys.argv[1])
